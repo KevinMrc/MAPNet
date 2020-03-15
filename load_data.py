7e56089dd671f507b4017cd603dc30ec7b0940f6
Kevin Mrc <43733329+KevinMrc@users.noreply.github.com>
2020-03-15 14:03:10 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -6,71 +6,56 @@ import random
 import cv2
 from sklearn.model_selection import train_test_split
 
-def load_batch(x, y):
-    x1 = []
-    y1 = []
-    for i in range(len(x)):
-        img = imageio.imread(x[i])
-        lab = imageio.imread(y[i])
-        img, lab = data_augmentation(img, lab)
-        lab = lab.reshape(512, 512, 1)
+
+def load_batch(images, labels, shape=64, channels=1, h_flip=True, vflip=True, rotation=True):
+    x1, y1 = [], []
+    for x, y in zip(images, labels):
+        # Load images
+        img = imageio.imread(x)
+        lab = imageio.imread(y)
+        # Augmentation
+        img, lab = data_augmentation(img, lab, h_flip, vflip, rotation)
+        # Reshape
+        lab = lab.reshape(shape, shape, 1)
+        img = img.reshape(shape, shape, channels)
+        # Mask verification
         lab[lab > 0.5] = 1
         lab[lab <= 0.5] = 0
+        # Store
         x1.append(img / 255.0)
         y1.append(lab)
-    y1 = np.array(y1).astype(np.float32)
-    return x1, y1
+    return x1, np.array(y1).astype(np.float32)
 
 
-def prepare_data():
-    # whu 512*512 4736
-    # img = np.array(sorted(
-    #     glob.glob(r'/media/lc/vge_lc/DL_DATE_BUILDING/WHU/cropped image tiles and raster labels/train/image/*.png')))
-    # label = np.array(sorted(
-    #     glob.glob(r'/media/lc/vge_lc/DL_DATE_BUILDING/WHU/cropped image tiles and raster labels/train/gt/*.png')))
-    #
-    # test_img = np.array(sorted(
-    #     glob.glob(r'/media/lc/vge_lc/DL_DATE_BUILDING/WHU/cropped image tiles and raster labels/test/image/*.png')))
-    # test_label = np.array(sorted(
-    #     glob.glob(r'/media/lc/vge_lc/DL_DATE_BUILDING/WHU/cropped image tiles and raster labels/test/gt/*.png')))
-    
-    
-    im = np.array(sorted(glob.glob(r'./AOI_3_Paris_Train/RGB-normalized/*.png')))
-    mask = np.array(sorted(glob.glob(r'./AOI_3_Paris_Train/segmentation/*.png')))
-    img, test_img, label, test_label = train_test_split(im, mask, test_size=0.33, random_state=42)
+def prepare_data(train_img_path, train_mask_path, test_image_path, test_mask_path):
 
+    img = np.array(sorted(glob.glob(r'{}'.format(train_img_path) + r'*.png')))[:3]
+    test_img = np.array(sorted(glob.glob(r'{}'.format(test_image_path) + r'*.png')))[:3]
+    label = np.array(sorted(glob.glob(r'{}'.format(train_mask_path) + r'*.png')))[:3]
+    test_label = np.array(sorted(glob.glob(r'{}'.format(test_mask_path) + r'*.png')))[:3]
 
-
-    # img = np.array(sorted(glob.glob(r'/media/lc/vge_lc/spacenet/train_rgb_image/*.png')))
-    # label = np.array(sorted(glob.glob(r'/media/lc/vge_lc/spacenet/train_label_image/*.png')))
-    # test_img=sorted(glob.glob(r'/media/lc/vge_lc/spacenet/shanghai_vegas_test_result/test_image/*.png'))
-    # test_label=sorted(glob.glob('/media/lc/vge_lc/spacenet/shanghai_vegas_test_result/test_label/*.png'))
-
-    # img = np.array(sorted(glob.glob(r'/home/lc/Jupyter_projects/resatt/Urban 3D Challenge Data/d_train/img/*.png')))
-    # label = np.array(sorted(glob.glob(r'/home/lc/Jupyter_projects/resatt/Urban 3D Challenge Data/d_train/gt/*.png')))
-    # test_img = np.array(sorted(glob.glob(r'/home/lc/Jupyter_projects/resatt/Urban 3D Challenge Data/d_test/img/*.png')))
-    # test_label = np.array(sorted(glob.glob(r'/home/lc/Jupyter_projects/resatt/Urban 3D Challenge Data/d_test/gt/*.png')))
     return img, label, test_img, test_label
 
 
-def data_augmentation(image, label):
+def data_augmentation(image, label, h_flip, vflip, rotation):
     # Data augmentation
 
-    if random.randint(0, 1):
+    if h_flip and random.randint(0, 1):
         image = np.fliplr(image)
         label = np.fliplr(label)
-    if random.randint(0, 1):
+
+    if vflip and random.randint(0, 1):
         image = np.flipud(image)
         label = np.flipud(label)
 
-    if random.randint(0,1):
+    if rotation and random.randint(0, 1):
         angle = random.randint(0, 3)*90
-        if angle!=0:
-            M = cv2.getRotationMatrix2D((image.shape[1] // 2, image.shape[0] // 2), angle, 1.0)
-            image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_NEAREST)
-            label = cv2.warpAffine(label, M, (label.shape[1], label.shape[0]), flags=cv2.INTER_NEAREST)
-    image_r = cv2.resize(image, (512, 512), interpolation = cv2.INTER_AREA) 
-    label_r = cv2.resize(label, (512, 512), interpolation = cv2.INTER_AREA) 
+        if angle != 0:
+            M = cv2.getRotationMatrix2D(
+                (image.shape[1] // 2, image.shape[0] // 2), angle, 1.0)
+            image = cv2.warpAffine(
+                image, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_NEAREST)
+            label = cv2.warpAffine(
+                label, M, (label.shape[1], label.shape[0]), flags=cv2.INTER_NEAREST)
 
-    return image_r, label_r
-
+    return image, label
--- train.py
+++ train.py
@@ -1,132 +1,179 @@
 import os
-import time
-# import utils
-import tensorflow as tf
-import numpy as np
-import skimage.io as io
-import argparse
+import re
 import cv2
-import matplotlib.pyplot as plt
+import time
+import argparse
+import numpy as np
+from PIL import Image
+import imageio
+import tensorflow as tf
 
-from load_data import load_batch,prepare_data
 from model.mapnet import mapnet
-# from model.hrnetv2 import hrnetv2
-# from model.pspnet import pspnet
-# from model.unet import unet
-# from model.resnet101 import resnet101
+from load_data import load_batch, prepare_data
 
-# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-
+# Args
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', type=int, default=2, help='Number of images in each batch')
-parser.add_argument('--learning_rate', type=float, default=0.001, help='Number of images in each batch')
 
+# Hyperparameters
+parser.add_argument('--batch_size', type=int, default=2,
+                    help='Number of images in each batch')
+parser.add_argument('--learning_rate', type=float,
+                    default=0.001, help='Number of images in each batch')
+parser.add_argument('--num_epochs', type=int, default=80,
+                    help='Number of epochs to train for')
+parser.add_argument('--weighted_loss', type=int, default=1,
+                    help="Weigth for positive detection in weighted cross entropy")
 
-parser.add_argument('--crop_height', type=int, default=512, help='Height of cropped input image to network')
-parser.add_argument('--crop_width', type=int, default=512, help='Width of cropped input image to network')
-parser.add_argument('--clip_size', type=int, default=450, help='Width of cropped input image to network')
-parser.add_argument('--num_epochs', type=int, default=80, help='Number of epochs to train for')
-parser.add_argument('--h_flip', type=bool, default=True, help='Whether to randomly flip the image horizontally for data augmentation')
-parser.add_argument('--v_flip', type=bool, default=True, help='Whether to randomly flip the image vertically for data augmentation')
-parser.add_argument('--color', type=bool, default=True, help='Whether to randomly flip the image vertically for data augmentation')
-parser.add_argument('--rotation', type=bool, default=True, help='randomly rotate, the imagemax rotation angle in degrees.')
-parser.add_argument('--start_valid', type=int, default=1, help='Number of epoch to valid')
-parser.add_argument('--valid_step', type=int, default=1, help="Number of step to validation")
+# Input size
+parser.add_argument('--crop_height', type=int, default=512,
+                    help='Height of cropped input image to network')
+parser.add_argument('--crop_width', type=int, default=512,
+                    help='Width of cropped input image to network')
+parser.add_argument('--channels', type=int, default=3,
+                    help="Number of channels in the input image")
 
+# Data augmentation
+parser.add_argument('--h_flip', type=bool, default=True,
+                    help='Whether to randomly flip the image horizontally for data augmentation')
+parser.add_argument('--v_flip', type=bool, default=True,
+                    help='Whether to randomly flip the image vertically for data augmentation')
+parser.add_argument('--rotation', type=bool, default=True,
+                    help='randomly rotate, the imagemax rotation angle in degrees.')
 
+# Validation
+parser.add_argument('--start_valid', type=int, default=0,
+                    help='Number of epoch to valid')
+parser.add_argument('--valid_step', type=int, default=1,
+                    help="Number of step to validation")
+
+# Folder paths
+parser.add_argument('--train_img_path', type=str, default='./image_64_sep/image/train/',
+                    help="Image train folder")
+parser.add_argument('--train_mask_path', type=str, default='./mask_64_sep/mask/train/',
+                    help="Mask train folder")
+parser.add_argument('--test_image_path', type=str, default='./image_64_sep/image/test/',
+                    help="Image test folder")
+parser.add_argument('--test_mask_path', type=str, default='./mask_64_sep/mask/test/',
+                    help="Mask test folder")
+
+parser.add_argument('--save_mask_train', type=bool, default=False,
+                    help="Save a mask during training")
 args = parser.parse_args()
-num_images=[]
-train_img, train_label,valid_img,valid_lab= prepare_data()
-num_batches=len(train_img)//(args.batch_size)
 
-img=tf.placeholder(tf.float32,[None,args.crop_height,args.crop_width,3])
-is_training=tf.placeholder(tf.bool)
-label=tf.placeholder(tf.float32,[None,args.crop_height,args.crop_height,1])
 
-pred=mapnet(img,is_training)
-pred1=tf.nn.sigmoid(pred)
+# Load data
+num_images = []
+train_img, train_label, valid_img, valid_lab = prepare_data(
+    args.train_img_path, args.train_mask_path, args.test_image_path, args.test_mask_path)
+num_batches = len(train_img) // (args.batch_size)
 
+# Placeholders
+img = tf.placeholder(
+    tf.float32, [None, args.crop_height, args.crop_width, args.channels])
+is_training = tf.placeholder(tf.bool)
+label = tf.placeholder(
+    tf.float32, [None, args.crop_height, args.crop_height, 1])
+pred = mapnet(img, is_training)
+pred1 = tf.nn.sigmoid(pred)
+
+# Loss and optimizer
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
-    sig=tf.nn.sigmoid_cross_entropy_with_logits(labels=label, logits=pred)
+    sig = tf.nn.weighted_cross_entropy_with_logits(
+        labels=label, logits=pred, pos_weight=args.weighted_loss)
     sigmoid_cross_entropy_loss = tf.reduce_mean(sig)
-    train_step = tf.train.AdamOptimizer(args.learning_rate).minimize(sigmoid_cross_entropy_loss)
-saver=tf.train.Saver(var_list=tf.global_variables())
+    train_step = tf.train.AdamOptimizer(
+        args.learning_rate).minimize(sigmoid_cross_entropy_loss)
+saver = tf.train.Saver(var_list=tf.global_variables())
 
 
 def load():
-    import re
+    """
+    Load checkpoints
+    """
     print("Reading checkpoints...")
     checkpoint_dir = './checkpoint/'
-
     ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+
     if ckpt and ckpt.model_checkpoint_path:
         ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
         saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
-        counter = int(next(re.finditer("(\d+)(?!.*\d)",ckpt_name)).group(0))
+        counter = int(next(re.finditer("(\d+)(?!.*\d)", ckpt_name)).group(0))
         print("Checkpoint {} read Successed".format(ckpt_name))
         return True, counter
     else:
-        print("Checkpoint not find ")
+        print("Checkpoint not found")
         return False, 0
 
+
 def train():
-
+    # Initialize
     tf.global_variables_initializer().run()
+    train_iter = []
+    train_loss = []
+    loss_tmp = []
+    IOU = 0.65
 
+    # Load checkpoints
     could_load, checkpoint_counter = load()
     if could_load:
         start_epoch = (int)(checkpoint_counter / num_batches)
         start_batch_id = checkpoint_counter - start_epoch * num_batches
         counter = checkpoint_counter
         print("Checkpoint Load Successed")
-
     else:
         start_epoch = 0
         start_batch_id = 0
         counter = 1
         print("train from scratch...")
 
-    train_iter=[]
-    train_loss=[]
-    IOU=0.65
-    # utils.count_params()
+    # Print info
     print("Total train image:{}".format(len(train_img)))
     print("Total validate image:{}".format(len(valid_img)))
     print("Total epoch:{}".format(args.num_epochs))
     print("Batch size:{}".format(args.batch_size))
     print("Learning rate:{}".format(args.learning_rate))
     #print("Checkpoint step:{}".format(args.checkpoint_step))
-
     print("Data Argument:")
     print("h_flip: {}".format(args.h_flip))
     print("v_flip: {}".format(args.v_flip))
     print("rotate: {}".format(args.rotation))
-    print("clip size: {}".format(args.clip_size))
-    loss_tmp = []
+
+    # Epoch
     for i in range(start_epoch, args.num_epochs):
-        epoch_time=time.time()
-        
+        epoch_time = time.time()
         id_list = np.random.permutation(len(train_img))
 
+        # Batch
         for j in range(start_batch_id, num_batches):
-            img_d = []
-            lab_d = []
-
+            img_d, lab_d = [], []
             for ind in range(args.batch_size):
                 id = id_list[j * args.batch_size + ind]
                 img_d.append(train_img[id])
                 lab_d.append(train_label[id])
 
-            x_batch, y_batch = load_batch(img_d, lab_d)
+            # Load
+            x_batch, y_batch = load_batch(
+                img_d, lab_d, shape=args.crop_height, channels=args.channels, 
+                h_flip=args.h_flip, vflip=args.v_flip, rotation=args.rotation)
+
             feed_dict = {img: x_batch,
                          label: y_batch,
-                         is_training:True
+                         is_training: True
                          }
+            # Train
+            _, loss, pred1 = sess.run(
+                [train_step, sigmoid_cross_entropy_loss, pred], feed_dict=feed_dict)
 
-            _, loss, pred1 = sess.run([train_step, sigmoid_cross_entropy_loss, pred], feed_dict=feed_dict)
-
+            # Save mask
+            if args.save_mask_train and (j % 100 == 0):
+                predict = pred1[1]
+                predict[predict < 0.5] = 0
+                predict[predict >= 0.5] = 1
+                result = np.squeeze(predict)
+                Image.fromarray(result*255.).convert("L").save('result_thresh_train.png')
+                
+            # Print loss
             loss_tmp.append(loss)
             if (counter % 100 == 0):
                 tmp = np.median(loss_tmp)
@@ -134,82 +181,86 @@ def train():
                 train_loss.append(tmp)
                 print('Epoch', i, '|Iter', counter, '|Loss', tmp)
                 loss_tmp.clear()
-
             counter += 1
         start_batch_id = 0
         print('Time:', time.time() - epoch_time)
 
         # saver.save(sess, './checkpoint/model.ckpt', global_step=counter)
 
-        
-        val_iou = validation()
-        print("last iou valu:{}".format(IOU))
-        print("new_iou value:{}".format(val_iou))
-        if val_iou > IOU:
-            print("Save the checkpoint...")
-            saver.save(sess, './checkpoint/model.ckpt', global_step=counter, write_meta_graph=True)
-            IOU = val_iou
+        # Validation
+        if i >= args.start_valid:
+            if (i - args.start_valid) % args.valid_step == 0:
+              val_iou = validation()
+              print("Last IOU value:{}".format(IOU))
+              print("New IOU value:{}".format(val_iou))
+
+              if val_iou > IOU:
+                  print("Save the checkpoint...")
+                  saver.save(sess, './checkpoint/model.ckpt',
+                            global_step=counter, write_meta_graph=True)
+                  IOU = val_iou
+
     saver.save(sess, './checkpoint/model.ckpt', global_step=counter)
 
-def f_iou(predict, label):
 
+def f_iou(predict, label):
     tp = np.sum(np.logical_and(predict == 1, label == 1))
-    fp = np.sum(predict==1)
+    fp = np.sum(predict == 1)
     fn = np.sum(label == 1)
-    return tp,fp+fn-tp
+    return tp, fp+fn-tp
 
 
 def validation():
-    sum_a = 0
-    sum_b = 0
-    print("validate...")
-    inter=0
-    unin=0
-    for j in range(0,len(valid_img)):
-        if (j % 100 == 0):
-          print(f'validation {j}')
+    print("Start validation...")
+    inter, unin = 0, 0
+
+    for j in range(0, len(valid_img)):
+        # Load image    
         x_batch = valid_img[j]
-        x_batch = io.imread(x_batch) / 255.0
-        x_batch_r = cv2.resize(x_batch, (512, 512), interpolation = cv2.INTER_AREA)
-        if (j % 100 == 0):
-          plt.figure()
-          plt.imshow(x_batch_r)
-          plt.savefig('x.png')
-        
-        x_batch = np.expand_dims(x_batch_r, axis=0)
+        x_batch = imageio.imread(x_batch) / 255.0
+
+        # Save image
+        if (j % 10000 == 0):
+            print(f'validation {j}')
+            if args.channels == 1:
+              Image.fromarray(x_batch*255.).convert("L").save('input_image_valid.png')
+            elif args.channels == 3:
+              Image.fromarray(x_batch*255.).convert("RGB").save('input_image_valid.png')
+
+        # Reshape
+        if args.channels == 1:
+            x_batch = x_batch[np.newaxis, :, :, np.newaxis]
+        elif args.channels == 3:
+            x_batch = x_batch[:, :, np.newaxis]
+
+        # Inference
         feed_dict = {img: x_batch,
-                     is_training:False
-
+                     is_training: False
                      }
-
         predict = sess.run(pred1, feed_dict=feed_dict)
-        sum_a += predict.sum()
+
+        # Mask
         predict[predict < 0.5] = 0
         predict[predict >= 0.5] = 1
-        sum_b += predict.sum()
         result = np.squeeze(predict)
-        if (j % 100 == 0):
-          plt.figure()
-          plt.imshow(result)
-          plt.savefig('result.png')
+        
+        # Get groud truth
+        gt_value = imageio.imread(valid_lab[j])
+        gt_value[gt_value < 0.5] = 0
+        gt_value[gt_value >= 0.5] = 1
 
-        gt_value = io.imread(valid_lab[j])
-        gt_value_r = cv2.resize(gt_value, (512, 512), interpolation = cv2.INTER_AREA)
-        gt_value_r[gt_value_r < 0.5] = 0
-        gt_value_r[gt_value_r >= 0.5] = 1
-        if (j % 100 == 0):
-            plt.figure()
-            plt.imshow(gt_value_r)
-            plt.savefig('gt_value_r_maks.png')
+        # Save mask and ground truth
+        if (j % 10000 == 0):
+            Image.fromarray(result*255.).convert("L").save('ouput_mask_valid.png')
+            Image.fromarray(gt_value*255).convert("L").save('ground_truth_mask_valid.png')
 
-        intr,unn=f_iou(gt_value_r,result)
+        # IOU
+        intr, unn = f_iou(gt_value, result)
+        inter = inter + intr
+        unin = unin + unn
 
-        inter=inter+intr
-        unin=unin+unn
-    print(sum_a, sum_b)
-    return inter*1.0/unin
+    return inter*1.0 / unin
 
 
 with tf.Session() as sess:
     train()
-
